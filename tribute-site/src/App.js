98801886bf196e1f219fb8c9c00e8bd5db0f18d9
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-01-31 17:45:15 -0800

    Construct Main.js to contain main content

--- tribute-site/src/App.js
+++ tribute-site/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
-import Header from '.components/Header'
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="app-wrapper">
+      <div className="content-wrapper">
+      <Header/>
+      <Main />
+      </div>
     </div>
   );
 }
--- tribute-site/src/components/Header.js
+++ tribute-site/src/components/Header.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React, {Component} from 'react';
 
 export default class Header extends Component {
@@ -6,7 +6,7 @@ export default class Header extends Component {
     return(
         <div>
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <p>
                 Hello world!
             </p>
@@ -16,4 +16,3 @@ export default class Header extends Component {
     }
 }
 
-export default Header;
