51e498ae134cb4e52a23f4d8f643706f60fd0668
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-03-10 13:55:46 -0800

    Resolve merge conflicts

--- tribute-site/src/App.js
+++ tribute-site/src/App.js
@@ -1,12 +1,9 @@
-
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 import './stylesheets/App.css';
-import Header from './components/Header.js'
 import { Component } from 'react';
 
 
@@ -15,7 +12,7 @@ class App extends Component {
   return (
     <div className="top-parent-container">
       <div className="header-wrapper">
-      <Header/>
+        <Header/>
       </div>
       <div className="main-wrapper">
         <Main />
